46a26f16486a5131e7b49c6fccd2476934a28d19
dcouch440 <dcouch440@gmail.com>
2021-10-08 19:17:52 -0700

    pass

---
+++ src/components/Card/CardTitle/index.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react';
+import CardTitle from '.';
+
+describe('Title', () => {
+  it('renders the title', () => {
+    render(<CardTitle text='testing' />);
+    const titleText = screen.getByText('testing');
+    expect(titleText).toBeInTheDocument();
+  });
+});
