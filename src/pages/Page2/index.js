cc1c65f2422153141b553cdd5b9302607062227a
dcouch440 <dcouch440@gmail.com>
2021-10-08 19:57:58 -0700

    create page

---
+++ src/pages/Page2/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PageHeader from '../../components/PageHeader';
+
+export default function Page2 () {
+  return (
+    <div>
+      <PageHeader text='Page 2' />
+    </div>
+  );
+}
---
+++ src/pages/Page3/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PageHeader from '../../components/PageHeader';
+
+export default function Page3 () {
+  return (
+    <div>
+      <PageHeader text='Page 3' />
+    </div>
+  );
+}
