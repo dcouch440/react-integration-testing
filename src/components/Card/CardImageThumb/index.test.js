c44b789ce2468edb0aced7ae882a54d11cc95663
dcouch440 <dcouch440@gmail.com>
2021-10-08 19:17:31 -0700

    pass

---
+++ src/components/Card/CardImageThumb/index.test.js
@@ -0,0 +1,11 @@
+import { render, screen } from '@testing-library/react';
+import ImageThumb from '.';
+
+describe('ImageThumb', () => {
+  it('should render a image on the page', () => {
+    render(<ImageThumb url={'/favicon.ico'} />);
+    const image = screen.getByRole('img');
+    expect(image).toBeInTheDocument();
+  });
+
+});
